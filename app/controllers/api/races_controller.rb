6f04ed221a661333b6ffc0f09e8de7bdc72ca7c8
guifigueiroa <guilherme.mfigueiroa@gmail.com>
2016-04-11 13:22:41 -0400

    Testing params with post and get resquests

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -4,7 +4,7 @@ module Api
     
     def index
       if !request.accept || request.accept == "*/*"
-        render plain: "/api/races"
+        render plain: "/api/races, offset=[#{params[:offset]}], limit=[#{params[:limit]}]"
       else
         ## TODO
       end
@@ -36,7 +36,7 @@ module Api
     
     def create
       if !request.accept || request.accept == "*/*"
-        render plain: :nothing, status: :ok
+        render plain: params[:race][:name]
       else
         ## TODO
       end
